d52fd58bdbabeba7bf84cc7afc6a8a972dff425f
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-03 10:47:39 +0530

    working on DataContent

---
+++ app/src/main/java/com/appbusters/robinkamboj/masterdetail/DataContent.java
@@ -0,0 +1,20 @@
+package com.appbusters.robinkamboj.masterdetail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by robinkamboj on 03/01/17.
+ */
+
+public class DataContent {
+
+
+    //An array of sample items
+    public static final List<Data> ITEMS = new ArrayList<Data>();
+
+    //A MAP od sample items, by id
+    public static final Map<String, Data> ITEM_MAP = new HashMap<String, Data>();
+}
