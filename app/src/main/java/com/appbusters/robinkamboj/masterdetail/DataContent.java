bc2903aeacf65f5d4cf935f262a9a9f46d757298
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-03 11:05:19 +0530

    DataContent

--- app/src/main/java/com/appbusters/robinkamboj/masterdetail/DataContent.java
+++ app/src/main/java/com/appbusters/robinkamboj/masterdetail/DataContent.java
@@ -13,10 +13,10 @@ public class DataContent {
 
 
     //An array of sample items
-    public static final List<Data> ITEMS = new ArrayList<Data>();
+    public static final List<createItem> ITEMS = new ArrayList<createItem>();
 
     //A MAP od sample items, by id
-    public static final Map<String, Data> ITEM_MAP = new HashMap<String, Data>();
+    public static final Map<String, createItem> ITEM_MAP = new HashMap<String, createItem>();
 
     //Number of items in recyclerview/list
     public static final int count=25;
@@ -24,14 +24,26 @@ public class DataContent {
     static {
         //add some sampleitems
         for(int i=1; i<=25; i++){
-
+            addItem(createItems(i));
         }
     }
 
-    private static void addItem(Data item){
+    private static void addItem(createItem item){
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
 
+    private static createItem createItems(int position){
+        return new createItem(String.valueOf(position), "Item" + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about item:").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
 
 }
--- app/src/main/java/com/appbusters/robinkamboj/masterdetail/createItem.java
+++ app/src/main/java/com/appbusters/robinkamboj/masterdetail/createItem.java
@@ -4,12 +4,12 @@ package com.appbusters.robinkamboj.masterdetail;
  * Created by robinkamboj on 03/01/17.
  */
 
-public class Data {
+public class createItem {
     public final String id;
     public final String content;
     public final String details;
 
-    public Data(String id, String content, String details) {
+    public createItem(String id, String content, String details) {
         this.id = id;
         this.content = content;
         this.details = details;
