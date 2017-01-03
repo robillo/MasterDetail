6e4231bd24ba52309445a324e98622d8c062946b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-03 10:52:56 +0530

    working on DataContent

--- app/src/main/java/com/appbusters/robinkamboj/masterdetail/DataContent.java
+++ app/src/main/java/com/appbusters/robinkamboj/masterdetail/DataContent.java
@@ -17,4 +17,21 @@ public class DataContent {
 
     //A MAP od sample items, by id
     public static final Map<String, Data> ITEM_MAP = new HashMap<String, Data>();
+
+    //Number of items in recyclerview/list
+    public static final int count=25;
+
+    static {
+        //add some sampleitems
+        for(int i=1; i<=25; i++){
+
+        }
+    }
+
+    private static void addItem(Data item){
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+
 }
